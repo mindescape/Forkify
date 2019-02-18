b8dc9a3a5c016a9fcd9a144e2e97dacaf47c9284
tomhet <escape.outerspace@gmail.com>
2019-02-18 20:18:32 +0500

    working on recipe module

---
+++ src/js/config.js
@@ -0,0 +1,1 @@
+export const key = 'db52682a0f67e5d014ec7914be9bb523';
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { key } from '../config';
 
 export default class Search {
   constructor(query) {
@@ -6,12 +7,11 @@ export default class Search {
   }
   
   async getResult() {
-    const key = 'db52682a0f67e5d014ec7914be9bb523';
     try {
       const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
       this.result = res.data.recipes;
     } catch (err) {
-      alert(err);
+      alert('Something went wrong :(');
     }
   }
 };
